856a26722e98ab7e9607b34c5af7772e40972881
Mac User <laser_yellow@cnc.jp>
2024-05-08 19:15:44 +0900

    API からもらった UTC のまま表示

--- app/src/main/java/com/armedcivil/aita/android/data/Reservation.kt
+++ app/src/main/java/com/armedcivil/aita/android/data/Reservation.kt
@@ -3,13 +3,12 @@ package com.armedcivil.aita.android.data
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 import java.math.BigInteger
-import java.util.Date
 
 @Parcelize
 data class Reservation(
     val id: BigInteger,
     val sheetId: String,
-    val startTimestamp: Date,
-    val endTimestamp: Date,
+    val startTimestamp: String,
+    val endTimestamp: String,
     val user: User,
 ) : Parcelable
